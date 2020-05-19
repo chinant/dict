77762b45846aa0028483b51e927bb09994c50598
samzw <zhouwei.china@gmail.com>
2020-05-19 15:53:15 +0800

    extract data from javascript array

--- dict.py
+++ dict.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
-import sys, re, pdb
+import sys, re
+import json
 from urllib.request import urlopen, Request
 from urllib.parse import quote
 
@@ -11,33 +11,35 @@ headers = {'User-Agent': user_agent}
 
 data = {}
 
+def save_to_file(file_name, contents):
+    fh = open(file_name, 'w')
+    fh.write(contents)
+    fh.close()
+
+
 if len(sys.argv) < 2:
     print('usage: %s <word>' % sys.argv[0])
 else:
     # word = re.sub('%20', '_', sys.argv[1]) #enable translate a sentence
     word = '%20'.join(sys.argv[1:]) #enable translate a sentence
     req = Request('http://iciba.com/word?w=%s' % quote(word),data,headers)
-    html = urlopen(req).read().decode('utf8')
-    soup = BeautifulSoup(html, 'html.parser')
-    context = soup.find("div", class_="Mean_mean__1ydUq")
-    if context is None:
-        print('Sorry, cannot translate the word(s)')
-    else:
-        speaks = context.find_all("ul", class_="Mean_symbols__5dQX7")
-        speak = speaks[0].find_all('li')
-        speak_result = ''
-        for line in  speak:
-            result = re.sub('\n', ' ', line.text)
-            #result = re.sub('&nbsp;', ' ', result)
-            speak_result += result.lstrip()
-            speak_result += ' '
-        print(speak_result)
 
-        ul = context.find_all("ul", class_="Mean_part__1RA2V")
-        rows = ul[0].find_all('li')
-        for row in rows:
-            # print(row.text)
-            result = re.sub('\n', ' ', row.text)
-            result = re.sub('&nbsp;', ' ', result)
-            result = re.sub(r'\s+', ' ', result)
-            print(result.strip())
+    try:
+        html = urlopen(req).read().decode('utf8')
+    except Exception:
+        raise("HTTPException")
+    
+    res_main = r'<script id="__NEXT_DATA__" type="application/json">(.*?)</script>'
+    main = re.findall(res_main,  html,re.S|re.M)
+   
+    data = json.loads(main[0])
+
+    if 'props' in data.keys() and 'symbols' in data['props']['initialDvaState']['word']['wordInfo']['baesInfo']:
+        symbols = data['props']['initialDvaState']['word']['wordInfo']['baesInfo']['symbols']
+        if (symbols):
+            print("英["+symbols[0]['ph_en'] + "]", "美["+symbols[0]['ph_am'] +"]")
+
+            for part in symbols[0]['parts']:
+                print(part['part'].ljust(3,' '), part['means'])
+    else:
+        print('Sorry, cannot translate the word(s)')
